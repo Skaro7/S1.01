5cfcfbd2c9a8e614c123ca369d99e48086cc234b
Skaro7 <marc_tau72@hotmail.com>
2025-12-09 18:22:39 +0100

    "docs: created abstract class Instruments with 2 attributes and abstract method play()"

--- src/Instruments.java
+++ src/Instruments.java
@@ -1,5 +1,11 @@
-public class Instruments {
-    // abstact class with two atributes name and price
-    // abstract method play()
-    //
+public abstract class Instruments {
+
+    protected String name;
+    protected double price;
+
+    public Instruments(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public abstract void play();
 }
